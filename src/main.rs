4cd91fd6ba7a551fcbb8ae9b83dd671105cccde2
= <punge3@gmail.com>
2021-05-11 00:00:54 -0400

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use std::fmt;
+use std::{fmt, fs::File, io::Write};
 
 use geo::Point;
 
 mod geo;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
     let pt = fetch_iss_location()?;
-    println!("{}", pt);
+    let kml = create_kml(&pt);
+
+    if args.len() > 1 {
+        let mut file = File::create(&args[1])?;
+        file.write_all(&kml.as_bytes())?;
+    } else {
+        println!("{}", kml)
+    }
+    
     Ok(())
 }
 
@@ -31,13 +40,43 @@ fn parse_point_from_json(json: &String) -> Result<geo::Point, FetchError> {
                 let lat = lat_string.unwrap().parse::<f64>().unwrap();
                 let lon = lon_string.unwrap().parse::<f64>().unwrap();
 
-            Ok(Point::new(lat, lon))
+                Ok(Point::new(lat, lon))
             } else {
                 return Err(FetchError::ParserError);
             }
         }
         Err(_) => return Err(FetchError::ParserError),
     }
+}
+
+fn create_kml(point: &geo::Point) -> String {
+    let kml = format!(
+        "<kml xmlns=\"http://www.opengis.net/kml/2.2\">
+    <Document>
+    <name>ISS Location</name>
+    <NetworkLink>
+    <Link>
+      <href>https://joeaustin.net/iss.kml</href>
+      <refreshMode>onInterval</refreshMode>
+      <refreshInterval>10</refreshInterval>
+    </Link>
+  </NetworkLink>
+    <Style id=\"default\">
+      <LabelStyle><scale>0</scale></LabelStyle>
+      <IconStyle><Icon>
+        <href>https://joeaustin.net/images/iss.png</href>
+        <hotSpot x=\"0.5\" y=\"0.5\" xunits=\"fraction\" yunits=\"fraction\"/>
+      </Icon></IconStyle>
+    </Style>
+    <Placemark>
+      <name>ISS</name>
+      <styleUrl>#default</styleUrl>
+      <Point><coordinates>{},{},0</coordinates></Point>
+    </Placemark>",
+        point.lon, point.lat
+    );
+
+    return kml;
 }
 
 #[derive(Debug)]
